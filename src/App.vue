ec64f3e6566e3f75ec47dbea20c791cdabd03fc0
Nahasa <mouler9@gmail.com>
2020-04-28 10:08:03 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>Hola</h1>
+    <p> {{message}} {{$store.state.counter}} </p>
+    <counter></counter>
+  </div>
+</template>
+
+<script>
+import Counter from '@/components/counter'
+export default {
+  name: 'app',
+  components: {
+    Counter
+  },
+  data () {
+    return {
+      message: 'HOLA COMPONENTE'
+    }
+  }
+}
+
+</script>
+
+<style scoped lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+</style>
---
+++ src/components/counter.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <button 
+            class="btn btn-success btn-block"
+            @click="increment(2)">
+            Aumentar contador
+        </button>
+        <div class="alert alert-secondary text-center mt-3">
+            {{counter}}
+        </div>
+        <button 
+            class="btn btn-primary btn-block"
+            @click="decrement()">
+            Disminuir contador
+        </button>
+    </div>
+</template>
+
+<script>
+    import {mapState, mapMutations} from 'vuex'
+    export default {
+        computed: {
+            ...mapState(['counter'])
+        },
+        methods: {
+            ...mapMutations(['increment','decrement'])
+        }
+    }
+</script>
+
+<style lang="stylus">
+
+</style>
